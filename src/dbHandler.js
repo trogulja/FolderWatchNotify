f0596b7cad2a713d0d1e568d9166c14746fadee8
Tibor Rogulja <tibor.rogulja@tiskarazagreb.hr>
2020-08-17 16:56:44 +0200

    test

--- src/dbHandler.js
+++ src/dbHandler.js
@@ -80,109 +80,127 @@ async function walk(dir) {
   ]);
 }
 
-// walk('C:\\Users\\Tibor\\AppData\\Local\\Temp\\test_folders').then(console.log);
+walk('Q:\\_JOBS').then((data) => {
+  const jobs = handleData(data);
+  console.log(jobs.filter((el) => el.todoNew));
+});
 
 // const rawdata = fs.readFile('output.json').then((data) => {
-//   const parsed = JSON.parse(data);
-//   handleData(parsed);
+//   handleData(JSON.parse(data));
 //   // console.log(parsed)
 // });
 
 function handleData(data) {
   const directories = data.filter((el) => el.type === 'directory').map((el) => el.path);
   const files = data.filter((el) => el.type === 'file').map((el) => el.path);
-  const jobs = handleDirectories(directories);
-  handleFiles(files, jobs);
-}
+  const garbage = { directories: [], files: [], extensions: new Set(), indd: new Set() };
+  const jobs = [];
+  const images = [];
 
-const rootDir = 'Q:\\_JOBS';
-
-function handleDirectories(directories) {
-  const output = [];
-  const garbage = [];
-
-  directories.forEach((directory) => {
-    // root dir == Q, _JOBS ... length == 2
-    const frag = directory.split(path.sep);
-    let job = {
-      root: 'Werbemarkt',
-      type: 'heatset',
-      profile: 'ISO Coated v2 300',
-      name: '1436-2006-Dasein02 Bilder',
-      path: 'Q:\\_JOBS\\Werbemarkt\\heatset\\ISO Coated v2 300\\1436-2006-Dasein02 Bilder',
-    };
-
-    if (frag.length < 3) return garbage.push({ str: directory, err: 'path length less than 3' });
-
-    if (frag[2] === 'Werbemarkt' || frag[2] === 'Others') {
-      if (frag[3] === 'heatset') {
-        if (frag.length !== 6)
-          return garbage.push({ str: directory, err: 'heatset path length not 6' });
-        job = { root: frag[2], type: frag[3], profile: frag[4], name: frag[5], path: directory };
-      } else if (frag[3] === 'coldset') {
-        if (frag.length !== 5)
-          return garbage.push({ str: directory, err: 'coldset path length not 5' });
-        job = {
-          root: frag[2],
-          type: frag[3],
-          profile: 'Newspaper Coldset V5',
-          name: frag[4],
-          path: directory,
-        };
-      } else {
-        return garbage.push({ str: directory, err: '#4 not heatset or coldset' });
-      }
-    } else {
-      return garbage.push({ str: directory, err: '#3 not Werbemarkt or Others' });
-    }
-
-    output.push(job);
+  directories.forEach((d) => {
+    const job = parsePath(d, 'directory');
+    if (job.err) return garbage.directories.push(job);
+    jobs.push({ ...job, todoNew: 0, todoTaken: 0, done: 0 });
   });
 
-  // console.log(output);
-  // const output2 = uniqBy(output, 'type');
-  // console.log('output2 len =', output2.length);
-  // console.log('output len =', output.length);
-
-  console.log(garbage); // handle garbage somehow
-  return output;
-}
-
-function handleFiles(files, jobs) {
-  const output = [];
-  const garbage = [];
-  files.forEach((file) => {
-    const frag = file.split(path.sep);
-    let find = {
-      root: 'Werbemarkt',
-      type: 'heatset',
-      profile: 'ISO Coated v2 300',
-      name: '1436-2006-Dasein02 Bilder',
-      file: 'something.psd',
-      path:
-        'Q:\\_JOBS\\Werbemarkt\\heatset\\ISO Coated v2 300\\1436-2006-Dasein02 Bilder\\TODO\\something.psd',
-    };
-
-    if (frag.length < 6) return garbage.push({ str: file, err: 'path length less than 6' });
-
-    if (frag[2] === 'Werbemarkt' || frag[2] === 'Others') {
-      if (frag[3] === 'heatset') {
-        find = { root: frag[2], type: frag[3], profile: frag[4], name: frag[5] };
-      } else if (frag[3] === 'coldset') {
-        // do the coldset
-      } else {
-        return garbage.push({ str: file, err: '#4 not heatset or coldset' });
-      }
-    } else {
-      return garbage.push({ str: file, err: '#3 not Werbemarkt or Others' });
+  files.forEach((f) => {
+    const item = parsePath(f, 'file');
+    if (item.err) return garbage.files.push(item);
+    if (!/\.(gif|jpe?g|tiff?|png|webp|bmp|psd|pdf|ai|eps|cdr|jfif|psb)$/i.test(f)) {
+      if (path.extname(f) === '.indd') return garbage.indd.add(path.basename(f, '.indd'));
+      if (path.basename(f) === 'Thumbs.db') return;
+      garbage.extensions.add(path.extname(f));
+      return garbage.files.push({ str: f, err: `extension is ignored` });
     }
+    const i = findIndex(jobs, {
+      root: item.root,
+      type: item.type,
+      profile: item.profile,
+      name: item.name,
+    });
+    if (i < 0) throw new Error('This file exists outside of directory structure!', item);
+
+    const status = parseFileStatus(f);
+    images.push({ ...item, id: i, status });
+    // jobs[i][status] += 1;
   });
+
+  const newImages = images.filter((el) => {
+    let res = true;
+    garbage.indd.forEach((e) => {
+      if (!res) return;
+      if (new RegExp(e).test(el.path)) res = false;
+    });
+    return res;
+  });
+  // console.log(jobs);
+  // console.log(`garbage dirs: ${garbage.directories.length}, garbage files: ${garbage.files.length}`);
+  // console.log(garbage.files);
+  // console.log(garbage.extensions);
+  return jobs;
 }
 
-var users = [
-  { user: 'barney', active: false, blob: 'kenja' },
-  { user: 'fred', active: false, blob: 'kenja' },
-  { user: 'pebbles', active: true, blob: 'kenja' },
-];
+function parsePath(fullpath, type) {
+  // fulpath == Q:\_JOBS\Werbemarkt\heatset\ISO Coated v2 300\1436-2006-Dasein02 Bilder\TODO\something.psd
+  // type    == file || directory
+  const frag = fullpath.split(path.sep);
 
-console.log(findIndex(users, { user: 'barney', blob: 'kenja', active: true }));
+  const lengthRoot = type === 'directory' ? 3 : 6;
+  if (frag.length < lengthRoot)
+    return { str: fullpath, err: `path length less than ${lengthRoot}` };
+  if (type !== 'directory' && type !== 'file')
+    return { str: fullpath, err: `type "${type}" is not known` };
+
+  if (frag[2] !== 'Werbemarkt' && frag[2] !== 'Others')
+    return { str: fullpath, err: '#3 not Werbemarkt or Others' };
+
+  // ANTENNE exception
+  if (frag[3] === 'ANTENNE') {
+    if (type === 'directory') {
+      if (frag.length !== 5) return { str: fullpath, err: 'ANTENNE dir path length not 5' };
+    } else {
+      if (frag.length <= 5) return { str: fullpath, err: 'ANTENNE file path length not 6+' };
+    }
+    return {
+      root: frag[2],
+      type: frag[3],
+      profile: 'Newspaper Coldset V5',
+      name: frag[4],
+      path: fullpath,
+    };
+  }
+
+  // Regular jobs
+  if (frag[3] !== 'coldset' && frag[3] !== 'heatset')
+    return { str: fullpath, err: '#4 not heatset or coldset' };
+
+  const lengthItem = frag[3] === 'heatset' ? 6 : 5;
+  if (type === 'directory') {
+    if (frag.length !== lengthItem)
+      return { str: fullpath, err: `${frag[3]} dir path length not ${lengthItem}` };
+  } else {
+    if (frag.length <= lengthItem)
+      return { str: fullpath, err: `${frag[3]} file path length not ${lengthItem + 1}+` };
+  }
+
+  const profile = frag[3] === 'heatset' ? frag[4] : 'Newspaper Coldset V5';
+  const name = frag[3] === 'heatset' ? frag[5] : frag[4];
+
+  return { root: frag[2], type: frag[3], profile, name, path: fullpath };
+}
+
+function parseFileStatus(fullpath) {
+  // const todo = new RegExp('\\.?to.?do.?\\', 'i');
+  // const todoDE = new RegExp('\\.?zu.?.?bearbeiten\\', 'i');
+  const done = new RegExp('\\\\.?done\\\\', 'i');
+  const doneDE = new RegExp('\\\\.?fertig\\\\', 'i');
+  const taken = new RegExp('\\\\.?taken', 'i');
+
+  let status = 'todoNew';
+
+  if (taken.test(fullpath)) status = 'todoTaken';
+  if (done.test(fullpath)) status = 'done';
+  if (doneDE.test(fullpath)) status = 'done';
+
+  return status;
+}
