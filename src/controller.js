8aac9d4f7fa72e86a46dd7fb7fb555d05a582187
Tibor Rogulja <tibor.rogulja@tiskarazagreb.hr>
2020-08-20 09:37:33 +0200

    test

---
+++ src/controller.js
@@ -0,0 +1,24 @@
+const ftp_hr_m4 = require('./jobFtphrm4');
+const database = require('./database');
+const db = new database();
+
+
+ftp_hr_m4.runme().then((data) => {
+  // data = { jobs: [{ root, type, profile, name, path, todoNew, todoTaken, done }], images: [{ jobID, status, path }] }
+  const cID = 'ftp_hr_m4';
+
+  db.delJob(cID);
+
+  data.jobs.forEach((job, i) => {
+    const id = db.addJob({ cID, ...job, dueMS: 0, updatedAtMS: new Date().getTime() });
+    data.jobs[i].id = id;
+  });
+  
+  db.addImages(
+    data.images.map((image) => ({
+      job: data.jobs[image.jobID].id,
+      status: image.status,
+      path: image.path,
+    }))
+  );
+});
---
+++ src/database.js
@@ -0,0 +1,36 @@
+const Database = require('better-sqlite3');
+const path = require('path');
+const paths = require('./pathHandler');
+// const db = new Database(path.join(paths.db, 'folderWatcher.db'), { verbose: console.log });
+
+class database {
+  constructor() {
+    this.db = new Database(path.join(paths.db, 'folderWatcher.db'));
+    this.db.pragma('journal_mode = WAL');
+    this.db.prepare('CREATE TABLE IF NOT EXISTS jobs (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, cID STRING, root STRING, type STRING, profile STRING, name STRING, todoNew INT, todoTaken INT, done INT, dueMS INT, updatedAtMS INT)').run();
+    this.db.prepare('CREATE TABLE IF NOT EXISTS files (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, job REFERENCES jobs(id) ON DELETE CASCADE ON UPDATE CASCADE, status STRING, path STRING)').run();
+
+    this.insertJob = this.db.prepare('INSERT INTO jobs (cID, root, type, profile, name, todoNew, todoTaken, done, dueMS, updatedAtMS) VALUES (@cID, @root, @type, @profile, @name, @todoNew, @todoTaken, @done, @dueMS, @updatedAtMS)');
+    this.deleteJob = this.db.prepare('DELETE FROM jobs WHERE cID = @cID')
+    this.insertImage = this.db.prepare('INSERT INTO files (job, status, path) VALUES (@job, @status, @path)');
+
+    const thisclass = this;
+    this.insertImages = this.db.transaction((images) => {
+      for (const image of images) thisclass.insertImage.run(image);
+    })
+  }
+
+  addJob(job) {
+    return this.insertJob.run(job).lastInsertRowid;
+  }
+
+  delJob(cID) {
+    return this.deleteJob.run({cID});
+  }
+
+  addImages(images) {
+    return this.insertImages(images);
+  }
+}
+
+module.exports = database;
